6aa2f5b58e60b419b439b80a36f6dcff595457c7
regs_dev <regs@usjr.edu.ph>
2017-07-18 21:10:12 +0800

    (6) Implement day and night theme modes

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,12 +1,16 @@
 package tech.hyperdev.scorekeeper.activities;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
+import tech.hyperdev.scorekeeper.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
-
         if(savedInstanceState == null) {
             scoreFragment1 = new ScoreFragment();
             scoreFragment2 = new ScoreFragment();
@@ -34,4 +38,27 @@ public class MainActivity extends AppCompatActivity {
             mFragmentTransaction.commit();
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.custom_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_night:
+                Utils.changeToTheme(MainActivity.this, 1);
+                break;
+            // action with ID action_settings was selected
+            case R.id.action_day:
+                Utils.changeToTheme(MainActivity.this, 0);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
 }
