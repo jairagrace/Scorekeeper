0043ba99b2ee77570eaeb4c5447d0bf3bb1e4cb9
regs_dev <regs@usjr.edu.ph>
2017-07-18 17:19:51 +0800

    (2) Infalte fragments for the two teams

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,15 +1,33 @@
 package tech.hyperdev.scorekeeper.activities;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import tech.hyperdev.scorekeeper.R;
+import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
+    private ScoreFragment scoreFragment1;
+    private ScoreFragment scoreFragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreFragment1 = new ScoreFragment();
+        scoreFragment2 = new ScoreFragment();
+
+        scoreFragment1.setTeamName("Team 1");
+        scoreFragment2.setTeamName("Team 2");
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.fragmentScore1, scoreFragment1);
+        mFragmentTransaction.replace(R.id.fragmentScore2, scoreFragment2);
+        mFragmentTransaction.commit();
     }
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
@@ -13,15 +14,45 @@ import tech.hyperdev.scorekeeper.R;
  */
 public class ScoreFragment extends Fragment {
 
+    private String teamname;
+    private int score;
+    private TextView mTeamName;
+    private TextView mScore;
     public ScoreFragment() {
-        // Required empty public constructor
+        this.score = 0;
+        this.teamname = "Team Name";
     }
-
+    public void setTeamName(String teamname){
+        this.teamname = teamname;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_score, container, false);
+        mTeamName = (TextView) rootView.findViewById(R.id.tvTeamName);
+        mScore = (TextView) rootView.findViewById(R.id.textView2);
+        View mBtnPlus = rootView.findViewById(R.id.btnPlus);
+        View mBtnMinus = rootView.findViewById(R.id.btnMinus);
+        mTeamName.setText(this.teamname);
+        mBtnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score++;
+                mScore.setText(score+"");
+            }
+        });
+        mBtnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(score-1 >= 0){
+                    score--;
+                    mScore.setText(score+"");
+                }
+            }
+        });
+        return rootView;
     }
 
 }
